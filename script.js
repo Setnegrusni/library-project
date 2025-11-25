02a4fa6ea9a367d6ed752988f4a4aa2ad121a771
Kenneth <krgutgar@gmail.com>
2025-11-24 20:01:51 -0600

    Finished logic for DOM manipulation

---
+++ script.js
@@ -0,0 +1,122 @@
+//Variables globales
+const myLibrary = [];
+const btFormBook = document.querySelector("#formBook");
+const myModal = document.querySelector("dialog");
+const btCloseModal = document.querySelector("#closeModal");
+const btAddBook = document.querySelector("#addBook");
+const bookTitle = document.querySelector("#bookTitle");
+const bookAuthor = document.querySelector("#bookAuthor");
+const bookPages = document.querySelector("#bookPages");
+const mainContent = document.querySelector(".main-container");
+let myLastBook, newCard, myCard, newPara;
+
+//=============================================================================
+//Eventos para botones
+//=============================================================================
+
+//Botón que muestra modal
+btFormBook.addEventListener("click", () => {
+    myModal.showModal();
+});
+
+//Botón que cierra modal
+btCloseModal.addEventListener("click", () => {
+    myModal.close();
+});
+
+//Botón que arega un nuevo libro
+btAddBook.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value);
+});
+
+//=============================================================================
+//Constructor de libros
+//=============================================================================
+function Book(title, author, pages) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+//=============================================================================
+//Funciones
+//=============================================================================
+
+//Crea un libro y lo agrega al arreglo myLibrary
+function addBookToLibrary(bookTitle, bookAuthor, bookPages) {
+    const addBook = new Book(bookTitle, bookAuthor, bookPages);
+
+    myLibrary.push(addBook);
+    myLastBook = myLibrary[myLibrary.length - 1]; //Último libro agregado
+    displayBooks();
+}
+
+//Se crean dos libros de ejemplo
+addBookToLibrary("Libro 1", "Autor 1", "255");
+addBookToLibrary("Libro 2", "Autor 2", "255");
+addBookToLibrary("Libro 3", "Autor 3", "255");
+addBookToLibrary("Libro 4", "Autor 4", "255");
+
+//Función que muestra los libros
+function displayBooks() {
+    createCard();
+}
+
+//Función que genera una nueva targeta
+function createCard() {
+    newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.setAttribute("data-id", myLastBook.id); //
+    mainContent.appendChild(newCard);
+    addCardElements();
+}
+
+//Función que agrega los elementos necesarios a la targeta
+function addCardElements() {
+    myCard = document.querySelector("[data-id='" + myLastBook.id + "']");
+    const btDelBook = document.createElement("button");
+
+    for (let i = 0; i <= 2; i++) {
+        newPara = document.createElement("p");
+
+        if (i === 0) {
+            newPara.classList.add("title");
+        } else if (i === 1) {
+            newPara.classList.add("author");
+        } else {
+            newPara.classList.add("pages");
+        }
+
+        myCard.appendChild(newPara);
+    }
+
+    btDelBook.setAttribute("id", "deleteBook");
+    btDelBook.setAttribute("data-card", myLastBook.id);
+    btDelBook.textContent = "Delete";
+    myCard.appendChild(btDelBook);
+
+    getCardInfo();
+
+    //Botón que elimina el libro
+    btDelBook.addEventListener("click", (e) => {
+        const cardId = e.target.dataset.card; //obtiene el ID a partir del atributo "card" del conjunto "data"
+        document.querySelector("[data-id='" + cardId + "']").remove(); //Elimina la targeta que coincide con ese ID
+        
+        //busca el índice del libro a eliminar a través de ID de la targeta
+        const indToDel = myLibrary.findIndex(book => book.id === cardId); 
+        myLibrary.splice(indToDel, 1); //Elimina el libro del arreglo
+    });
+}
+
+//Agrega la información correspondiente a la targeta partir del id 
+function getCardInfo() {
+    myParaTitle = document.querySelector("[data-id='" + myLastBook.id + "'] > .title");
+    myParaAuthor = document.querySelector("[data-id='" + myLastBook.id + "'] > .author");
+    myParaPages = document.querySelector("[data-id='" + myLastBook.id + "'] > .pages");
+
+    myParaTitle.textContent = myLastBook.title
+    myParaAuthor.textContent = myLastBook.author
+    myParaPages.textContent = myLastBook.pages
+}
